546c29c6c13c9c08a20596896b5f0cc48eb4d45e
Cho Kisung <kisung128@gmail.com>
2025-10-27 12:38:05 +0900

    grass에 dx,dy로 생성시 위치 조절 기능 추가

--- Lecture10_Game_World/grass.py
+++ Lecture10_Game_World/grass.py
@@ -2,11 +2,13 @@ from pico2d import load_image
 
 
 class Grass:
-    def __init__(self):
+    def __init__(self, dx = 0, dy = 0):
         self.image = load_image('grass.png')
+        self. x = 400 + dx
+        self. y = 30 + dy
 
     def draw(self):
-        self.image.draw(400, 30)
+        self.image.draw(self.x, self.y)
 
     def update(self):
         pass
